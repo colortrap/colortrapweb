99593c1440ba362c9dd8fbb95c267390007930d3
Martina Valchanova <colortrap.ltd@gmail.com>
2026-01-15 16:14:30 +0200

    update calendar items number

--- src/main/java/com/colortrap/web/service/WorkshopService.java
+++ src/main/java/com/colortrap/web/service/WorkshopService.java
@@ -36,8 +36,8 @@ public class WorkshopService {
         if (views.isEmpty()){
             views.add(contentProvider.getDefaultWorkshopVew("notfound"));
         }
-        while (views.size() > 36){
-            views.remove(36);
+        while (views.size() > 41){
+            views.remove(41);
         }
         return views;
     }
